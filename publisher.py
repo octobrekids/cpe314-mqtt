cf23ed4471b383cb78162834f9b60b0db17ac88f
octobrekids <chnrd.k@gmail.com>
2020-03-07 23:40:07 +0700

    publisher no bug bug bug

--- publisher.py
+++ publisher.py
@@ -7,50 +7,47 @@ SERV_PORT = 50000
 role = "publisher"
 
 def main(): 
+  try:
+    print('PUBLISHER CMD')
+    print('publish (broker_ip_address) (topic_name) (data to publish)')
+    print("ex. publish 127.0.0.1 '/room1/light' 'value=on' ")  
 
-  print('PUBLISHER CMD')
-  print('publish (broker_ip_address) (topic_name) (data to publish) ---------- to publish a data')
-  print("ex. publish 127.0.0.1 '/room1/light' 'value=on' ")  
 
-  while True:
-      print("command > " ,end=" ")
-      sys.stdout.flush()
-      command = sys.stdin.readline().strip()
-    
-      # to lower and split the command
-      cmdSplit =  command.lower().split(" ")
+    while True:
+        print("command > " ,end=" ")
+        sys.stdout.flush()
+        command = sys.stdin.readline().strip()
       
-      # quit
-      if cmdSplit[0] == 'quit':
-        break
-      # wrong format of command
-      elif len(cmdSplit) != 4 or cmdSplit[0] != 'publish':
-        print("SyntaxError : Wrong Format of Command")
-        print("length after %s " %(len(cmdSplit)))
-      else:
-        print("true command : %s" %(command))
-        broker_ip_address = cmdSplit[1]
-        print(broker_ip_address)
-
-        # connect broker ip socket
-        serv_sock_addr = (broker_ip_address, SERV_PORT)
-        cli_sock = socket(AF_INET, SOCK_STREAM) # tcp socket
+        # to lower and split the command
+        cmdSplit =  command.lower().split(" ")
         
-        # set socket timeout
-        cli_sock.settimeout(2.0)  
+        # quit
+        if command == 'quit':
+          print('disconnect....')
+          break
+        # wrong format of command
+        elif len(cmdSplit) == 4 and cmdSplit[0] == 'publish':
+          broker_ip_address = cmdSplit[1]
 
-        try:
-          cli_sock.connect(serv_sock_addr)
-          cli_sock.send(role.encode('utf-8'))
-          cli_sock.send(command.encode('utf-8'))
-          confirmMsg = cli_sock.recv(2048)
-          print(confirmMsg.decode('utf-8'))
+          # connect broker ip socket
+          serv_sock_addr = (broker_ip_address, SERV_PORT)
+         
+          try:
+            cli_sock = socket(AF_INET, SOCK_STREAM) # tcp socket
+            cli_sock.settimeout(2.0)  
+            cli_sock.connect(serv_sock_addr)
+            cli_sock.send(command.encode('utf-8'))
+            confirmMsg = cli_sock.recv(2048)
+            print(confirmMsg.decode('utf-8'))
 
-        except: 
-          print('NoBrokerIP: Connection Failed')
-          cli_sock.close()
+          except: 
+            print('NoBrokerIP: Connection Failed')
 
-  cli_sock.close()
+        else:
+          print("SyntaxError : Wrong Format of Command")
+
+  except:
+    cli_sock.close()
 
 if __name__ == '__main__': 
    try:
